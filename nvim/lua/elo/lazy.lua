d8c845b04b111c0ac90d5bb66e8db7b8144683ee
elias <rojas.elias@outlook.com>
2024-01-04 16:43:14 -0300

    morocha

--- nvim/lua/elo/lazy.lua
+++ nvim/lua/elo/lazy.lua
@@ -28,4 +28,6 @@ require("lazy").setup({
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',	
+    -- colorschemes
+    'yorickpeterse/vim-paper'
 })
--- nvim/lua/elo/set.lua
+++ nvim/lua/elo/set.lua
@@ -19,6 +19,8 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
+vim.opt.termguicolors = true
+
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -32,4 +34,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-vim.cmd("colorscheme quiet")
+vim.opt.guicursor = ""
+
+vim.cmd('set background=light')
+vim.cmd('colorscheme paper')
