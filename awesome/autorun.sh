3b97e47f3d812e994fc1081c7b615647ad377d4f
elias rojas <nerfelo@elomachine.rojas.com>
2024-01-30 11:05:13 -0300

    awesome

---
+++ awesome/autorun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+run() {
+    if ! pgrep -f "$1"; then # si no hay un proceso con el nombre del programa
+        "$@" & # tira el programa con sus argumentos en background
+    fi
+}
+run setxkbmap -option caps:swapescape
+run flameshot
