c6faba2f7dbf5656657918bfd1c4d62b28d8e9f7
Elias Rojas <rojas.elias@outlook.com>
2025-02-13 23:27:57 -0300

    sapee

--- nvim/lua/elo/plugin/telescope.lua
+++ nvim/lua/elo/plugin/telescope.lua
@@ -7,6 +7,7 @@ return {
       fzf.setup({'fzf-native'})
       vim.keymap.set('n', '<leader>ff', fzf.files, {})
       vim.keymap.set('n', '<leader>fg', fzf.grep, {})
+      vim.keymap.set('n', '<leader>fj', fzf.git_files, {})
       vim.keymap.set('n', '<leader>fb', fzf.buffers, {})
     end
   }
